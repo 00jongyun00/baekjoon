9ab1c669c6db271779a2da8c9a726ed6451983d6
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-10-01 18:11:36 +0900

    체스판 다시 칠하기

--- 재귀/11729.go
+++ 재귀/11729.go
@@ -9,13 +9,10 @@ import (
 
 func hanoi(n int, from int, mid int, to int, wr *bufio.Writer) {
 	if n == 1 {
-		//fmt.Fprintf(wr, "원반 %d을 %d에서 %d로 이동\n", n, from, to)
 		fmt.Fprintf(wr, "%d %d\n", from, to)
 		return
 	}
-	// 시작 -> 경유지 -> 도착
 	hanoi(n-1, from, to, mid, wr)
-	//fmt.Fprintf(wr, "원반 %d를 %d에서 %d로 이동\n", n, from, to)
 	fmt.Fprintf(wr, "%d %d\n", from, to)
 	hanoi(n-1, mid, from, to, wr)
 }
